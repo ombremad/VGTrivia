2ffe66fb36dde1a461d2061da97583024d56b801
Anne Ferret <anne@anneferret.eu>
2025-08-18 19:33:18 +0200

    Initial Commit

---
+++ VGTrivia/VGTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VGTriviaApp.swift
+//  VGTrivia
+//
+//  Created by Anne Ferret on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct VGTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
