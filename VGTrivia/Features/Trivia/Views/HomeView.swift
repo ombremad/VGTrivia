d7ed9833449657b993b337ca4c430ef771b6c9cb
Anne Ferret <anne@anneferret.eu>
2025-08-26 13:44:04 +0200

    HomeView subviews refactoring

--- VGTrivia/Features/Trivia/Views/HomeView.swift
+++ VGTrivia/Features/Trivia/Views/HomeView.swift
@@ -10,32 +10,42 @@ import SwiftUI
 struct HomeView: View {
     @Environment(TriviaViewModel.self) var triviaViewModel
     
+    private func numberOfQuestions() -> some View {
+        VStack(spacing: 20) {
+            Text("Number of questions")
+                .font(.appTitle)
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 10) {
+                ForEach(Array(stride(from: 5, to: 31, by: 5)), id: \.self) { index in
+                            Button(action: {
+                                triviaViewModel.roundLength = index
+                            }) {
+                                Text(index.description)
+                            }
+                            .buttonStyle(TriviaButton(backgroundColor: triviaViewModel.roundLength == index ? .babyBlue : .pearl))
+                }
+            }
+        }
+    }
+    private func otherSettings() -> some View {
+        VStack(spacing: 20) {
+            Text("Other settings")
+                .font(.appTitle)
+            Text("(To be implemented soon-ish)")
+        }
+    }
+    private func startButton() -> some View {
+        NavigationLink (destination:RoundView()) {
+            Text("Start game")
+                .font(.appTitle)
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             VStack(spacing: 50) {
-                VStack(spacing: 20) {
-                    Text("Number of questions")
-                        .font(.appTitle)
-                    LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 10) {
-                        ForEach(Array(stride(from: 5, to: 31, by: 5)), id: \.self) { index in
-                                    Button(action: {
-                                        triviaViewModel.roundLength = index
-                                    }) {
-                                        Text(index.description)
-                                    }
-                                    .buttonStyle(TriviaButton(backgroundColor: triviaViewModel.roundLength == index ? .babyBlue : .pearl))
-                        }
-                    }
-                }
-                VStack(spacing: 20) {
-                    Text("Other settings")
-                        .font(.appTitle)
-                    Text("(To be implemented soon-ish)")
-                }
-                NavigationLink (destination:RoundView()) {
-                    Text("Start game")
-                        .font(.appTitle)
-                }
+                numberOfQuestions()
+                otherSettings()
+                startButton()
             }
             .padding(.horizontal)
             .onAppear {
